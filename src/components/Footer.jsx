5682fa08d9803f46118f5a844e6d786e413b16a2
Benjamin Leguizamon <leguizamonbenjamin0@gmail.com>
2022-11-06 23:43:11 -0300

    algunas modificaciones en la vista mobile a ver si queda bien el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import {BsLinkedin, BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div className='backdrop-blur-md grid md:grid-cols-4 justify-items-center sm:grid-cols-1 py-7 mt-10 object-bottom w-full'>
+    <div className='backdrop-blur-md grid md:grid-cols-4 justify-items-center sm:grid-cols-1 py-7 mt-10 relative bottom-0 w-full'>
         <div className='text-lg text-white font-semibold my-auto sm:my-3 text-center'>
             <p>Benjamin Leguizamon.</p>
         </div>
